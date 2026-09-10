d62da5f0a72f46b23e1fb6b886827095fdc77227
maraaa347 <amararamsay@gmail.com>
2026-09-10 12:25:26 -0400

    debug and add variables about to train model using Tree.

--- archive (1)/miami_housing.py
+++ archive (1)/miami_housing.py
@@ -3,11 +3,31 @@
 from pathlib import Path
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
 
-csv_path = Path(__file__).with_name("miami-housing.csv")
-df = pd.read_csv(csv_path)
-#print(df.isnull().sum())  # Check for missing values in the DataFrame
+miami_data_path = Path(__file__).with_name("miami-housing.csv")
+df = pd.read_csv(miami_data_path)
 df = df.dropna()  # Drop rows with missing values
-print(df.head(10))  # Display the first few rows of the DataFrame
+#print(df.head(10))  # Display the first 10 rows of the DataFrame
+miami_data = df
+miami_data.describe()
+
+y = miami_data.SALE_PRC
+#Choosing Features 
+miami_features = ['LND_SQFOOT', 'TOT_LVG_AREA', 'SPEC_FEAT_VAL', 'age', 'LATITUDE', 'LONGITUDE']
+x = miami_data[miami_features]
+x_train, x_test, y_train, y_test = train_test_split(
+	x, y, test_size=0.2, random_state=1
+)
+model = DecisionTreeRegressor(random_state=1)
+
+#Fit model 
+model.fit(x_train, y_train)
+
+#Testing the model rq
+print("Making predictions for the following 5 houses:")
+print(x_test.head())
+print("The predictions are")
+print(model.predict(x_test.head()))
 
 
