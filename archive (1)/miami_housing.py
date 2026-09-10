0159af4790fc2afefd9dfee28413eb424d8d56bf
maraaa347 <amararamsay@gmail.com>
2026-09-10 15:52:02 -0400

    Calculate MAE and predictions

--- archive (1)/miami_housing.py
+++ archive (1)/miami_housing.py
@@ -15,19 +15,38 @@ miami_data.describe()
 y = miami_data.SALE_PRC
 #Choosing Features 
 miami_features = ['LND_SQFOOT', 'TOT_LVG_AREA', 'SPEC_FEAT_VAL', 'age', 'LATITUDE', 'LONGITUDE']
+#Selecting x to model the features
 x = miami_data[miami_features]
-x_train, x_test, y_train, y_test = train_test_split(
-	x, y, test_size=0.2, random_state=1
-)
+
+#Define model
 model = DecisionTreeRegressor(random_state=1)
 
 #Fit model 
-model.fit(x_train, y_train)
+model.fit(x, y)
 
 #Testing the model rq
 print("Making predictions for the following 5 houses:")
-print(x_test.head())
+print(x.head())
 print("The predictions are")
-print(model.predict(x_test.head()))
+print(model.predict(x.head()))
 
+#Model Validation using Mean Absolute Error (MAE)
+#To calculate the MAE
+from sklearn.metrics import mean_absolute_error
+predictions = model.predict(x)
+mae = mean_absolute_error(y, predictions)
+print(f"Mean Absolute Error: {mae}")
 
+#Import train_test_split to split the data into training and testing sets
+x_train, x_valid, y_train, y_valid = train_test_split(
+	x, y, test_size=0.25, random_state=1
+)
+#Define model 
+model = DecisionTreeRegressor(random_state=1)
+#Model Fit 
+model.fit(x_train, y_train)
+
+#Get predicted results from the validation data 
+val_predictions = model.predict(x_valid)
+print("Validation Predictions:")
+print(mean_absolute_error(y_valid, val_predictions))
